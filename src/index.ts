41f4df2b4db875984b815b8655d893c9fd99c326
omniscient <17525998+omnisci3nce@users.noreply.github.com>
2021-04-04 21:18:00 +1000

    start on first chapter

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,33 @@
-console.log('WhATS UP');
+import * as fs from 'fs';
 
+class Lox {
+  constructor() {}
+
+  public static main(args: string[]) : void {
+    if (args.length > 1) {
+      console.log('Usage: jlox [script]');
+      return;
+    } else if (args.length === 1) {
+      this.runFile(args[0]);
+    } else {
+      this.runPrompt();
+    }
+  }
+
+  private static runFile(path: string) : void {
+    let source;
+    
+    try {
+      source = fs.readFileSync(path, { encoding: 'utf-8' });
+    } catch (err) {
+      console.error(`Error :O`);
+      process.exit(64);
+    }
+  }
+
+  private static runPrompt() : void {
+
+  }
+}
+
+Lox.main(process.argv.slice(3));
