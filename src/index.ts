264c7c02eb1acddda1af3ec97d5476efa5cd6e06
omniscient <17525998+omnisci3nce@users.noreply.github.com>
2021-05-10 22:41:37 +1000

    yes

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,65 @@
 import * as fs from 'fs';
+import { createInterface } from 'readline';
+
+const readline = createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+enum TokenType {
+  // Single-character tokens
+  LEFT_PAREN, RIGHT_PAREN, LEFT_BRACE, RIGHT_BRACE,
+  COMMA, DOT, MINUS, PLUS, SEMICOLON, SLASH, STAR,
+
+  // One or two character tokens
+  BANG, BANG_EQUAL
+  EQUAL, EQUAL_EQUAL,
+  GREATER, GREATER_EQUAL,
+  LESS, LESS_EQUAL,
+
+  // Literals
+  IDENTIFIER, STRING, NUMBER,
+
+  // Keywords
+  AND, CLASS, ELSE, FALSE, FUN, FOR, IF, NIL, OR,
+  PRINT, RETURN, SUPER, THIS, TRUE, VAR, WHILE,
+
+  EOF
+};
+
+class Token {
+  type: TokenType;
+  lexeme: string;
+  literal: Object,
+  line: number;
+
+  constructor(type: TokenType, lexeme: string, literal: Object, line: number) {
+    this.type = type;
+    this.lexeme = lexeme;
+    this.literal = literal;
+    this.line = line;
+  }
+
+  toString() {
+    // fill
+  }
+}
+
+class Scanner {
+  source: string;
+
+  constructor(source: string) {
+    this.source = source;
+  }
+
+  scanTokens() : Token[] {
+    return [];
+  }
+}
 
 class Lox {
+  static hadError: boolean = false;
+
   constructor() {}
 
   public static main(args: string[]) : void {
@@ -23,10 +82,29 @@ class Lox {
       console.error(`Error :O`);
       process.exit(64);
     }
+
+    run(source);
   }
 
-  private static runPrompt() : void {
+  private static runPrompt() : void {}
 
+  private static run(source: string) {
+    const scanner = new Scanner(source);
+    const tokens = scanner.scanTokens();
+
+    // print tokens
+    tokens.forEach((token: Token) => {
+      console.log(token);
+    });
+  }
+
+  static error(line: number, message: string) : void {
+    this.report(line, '', message);
+  }
+
+  private static report(line: number, where: string, message: string) : void {
+    console.log('[line ' + line + '] Error' + where + ': ' + message);
+    this.hadError = true;
   }
 }
 
