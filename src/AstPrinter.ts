272aba9bdde909a24044428770ba2acc4ac320a6
omnisci3nce <17525998+omnisci3nce@users.noreply.github.com>
2021-08-22 07:08:32 +1000

    visitor pattern compiles!? uwu

--- src/AstPrinter.ts
+++ src/AstPrinter.ts
@@ -1,5 +1,30 @@
-import { Expr, Visitor } from './Expr'
+import { Expr, Visitor, Binary, Literal, Grouping, Unary } from './Expr'
 
-class AstPrinter implements Vistor<String> {
+class AstPrinter implements Visitor<string> {
+  public print(expr: Expr): string {
+    return expr.accept(this);
+  }
 
+  public visitLiteralExpr(expr: Literal): string {
+    return "";
+  }
+
+  public visitUnaryExpr(expr: Unary): string {
+    // return this.parenthesise(expr.operator.lexeme, [expr.left, expr.right]);
+    return "";
+  }
+
+  public visitBinaryExpr(expr: Binary): string {
+    // return this.parenthesise(expr.operator.lexeme, [expr.left, expr.right]);
+    return "";
+  }
+
+  public visitGroupingExpr(expr: Grouping): string {
+    // return this.parenthesise(expr.operator.lexeme, [expr.left, expr.right]);
+    return "";
+  }
+
+  private parenthesise(name: string, subExpressions: Expr[]): string {
+    return "";
+  }
 }
--- src/Expr.ts
+++ src/Expr.ts
@@ -1,35 +1,29 @@
 import { Token } from './index'
 
-interface Visitor<R> {}
-
-/*class Visitor {
-  public visitBinary(expr: Binary): any {
-    return true
-  }
-}
-*/
-
-abstract class Expr {
-  public abstract accept(visitor: Visitor): any;
+export interface Visitor<R> {
+  visitLiteralExpr(expr: Literal): R;
+  visitUnaryExpr(expr: Unary): R;
+  visitBinaryExpr(expr: Binary): R;
+  visitGroupingExpr(expr: Grouping): R;
 }
 
-export class Binary extends Expr {
-  constructor(public left: Expr, public operator: Token, public right: Expr) {
-    super();
-  }
-  public accept(visitor: Visitor<R>) {
-    return visitor.visitBinary(this);
-  }
+// class Visitor implements IVisitor<string> {
+//   public visitBinary(expr: Binary): any {
+//     return true
+//   }
+// }
+
+export abstract class Expr {
+  public abstract accept<R>(visitor: Visitor<R>): R;
 }
-/*
-export class Grouping extends Expr {
-  constructor(public expression: Expr) {
-    super();
-  }
-}
+
 export class Literal extends Expr {
   constructor(public value: any) {
     super();
+  }
+
+  public accept<R>(visitor: Visitor<R>) {
+    return visitor.visitLiteralExpr(this);
   }
 }
 
@@ -37,6 +31,27 @@ export class Unary extends Expr {
   constructor(public operator: Token, public right: Expr) {
     super();
   }
-}
-*/
 
+  public accept<R>(visitor: Visitor<R>) {
+    return visitor.visitUnaryExpr(this);
+  }
+}
+export class Binary extends Expr {
+  constructor(public left: Expr, public operator: Token, public right: Expr) {
+    super();
+  }
+
+  public accept<R>(visitor: Visitor<R>) {
+    return visitor.visitBinaryExpr(this);
+  }
+}
+
+export class Grouping extends Expr {
+  constructor(public expression: Expr) {
+    super();
+  }
+
+  public accept<R>(visitor: Visitor<R>) {
+    return visitor.visitGroupingExpr(this);
+  }
+}
